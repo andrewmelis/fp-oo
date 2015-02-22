a1555a983cdacb83f67b4dea98b84ae196cd350e
Andrew Melis <andrewmelis@gmail.com>
2015-02-21 21:54:52 -0500

    setup ch3

---
+++ src/fp_oo/add_and_make.clj
@@ -0,0 +1,8 @@
+(ns fp-oo.add-and-make)
+
+(def Point
+  (fn [x y]
+    {:x x
+     :y y}))
+
+
---
+++ test/fp_oo/add_and_make_test.clj
@@ -0,0 +1,3 @@
+(ns fp-oo.add-and-make-test
+  (require [clojure.test :refer :all]
+           [fp-oo.add-and-make :refer :all]))
