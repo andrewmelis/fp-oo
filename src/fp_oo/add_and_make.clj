f710a8bcb526c27450eee882d278516b8f7d6d94
Andrew Melis <andrewmelis@gmail.com>
2015-02-21 22:27:19 -0500

    add ch3 givens with tests

--- src/fp_oo/add_and_make.clj
+++ src/fp_oo/add_and_make.clj
@@ -3,6 +3,23 @@
 (def Point
   (fn [x y]
     {:x x
-     :y y}))
+     :y y
+     :__class_symbol__ 'Point}))
 
+(def verbose-y
+  (fn [this]
+    (get this :y)))
 
+(def shorter-y
+  (fn [this]
+    (:y this)))
+
+(def y :y) ;; first elt of list is keyword, therefore callable
+(def x :x)
+
+(def class-of :__class_symbol__)
+
+(def shift
+  (fn [this xinc yinc]
+    (Point (+ (x this) xinc)
+           (+ (y this) yinc))))
--- test/fp_oo/add_and_make_test.clj
+++ test/fp_oo/add_and_make_test.clj
@@ -1,3 +1,27 @@
 (ns fp-oo.add-and-make-test
   (require [clojure.test :refer :all]
            [fp-oo.add-and-make :refer :all]))
+
+(deftest Point-constructor-test
+  (testing "can construct a Point and access 'instance variables'"
+    (is (= 2 (:y (Point 1 2))))))
+
+(deftest getter-methods-test
+  (testing "verbose getter"
+    (is (= 2 (verbose-y (Point 1 2)))))
+  (testing "shorter getter"
+    (is (= 2 (shorter-y (Point 1 2)))))
+  (testing "shortest getter"
+    (is (= 2 (y (Point 1 2))))
+    (is (= 1 (x (Point 1 2))))))
+
+
+(deftest class-of-test
+  (testing "returns symbol for 'object'"
+    (is (= 'Point (class-of (Point 1 2))))))
+
+(deftest shift-test
+  (testing "returns a new point with coordinates 'updated' by arguments"
+    (is (= (Point 0 0)
+           (shift (Point 1 200) -1 -200)))))
+
