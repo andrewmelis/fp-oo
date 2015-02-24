4a6bf86adb0a12a8587863df982fefc7f47d43c4
Andrew Melis <andrewmelis@gmail.com>
2015-02-23 23:34:37 -0600

    add ch5 givens with tests

--- src/fp_oo/ch5_class.clj
+++ src/fp_oo/ch5_class.clj
@@ -1,1 +1,40 @@
 (ns fp-oo.ch5-class)
+
+(def verbose-make
+  (fn [class & args]
+    (let [allocated {}
+          seeded (assoc allocated
+                        :__class_symbol__ (:__own_symbol__ class))
+          constructor (:add-instance-values
+                        (:__instance_methods__ class))]
+      (apply constructor seeded args))))
+
+(def make
+  (fn [class & args]
+    (let [seeded {:__class_symbol__ (:__own_symbol__ class)}
+          constructor (:add-instance-values (:__instance_methods__ class))]
+      (apply constructor seeded args))))
+
+(def send-to
+  (fn [instance message & args]
+    (let [class (eval (:__class_symbol__ instance))
+          method (message (:__instance_methods__ class))]
+      (apply method instance args))))
+
+(def Point
+  {
+   :__own_symbol__ 'Point   ;; or `Point? -- that's a backtick
+   :__instance_methods__
+   {
+    :add-instance-values
+    (fn [this x y]
+      (assoc this :x x :y y))
+
+    :class :__class_symbol__
+
+    :shift
+    (fn [this xinc yinc]
+      (make Point (+ (:x this) xinc)
+                  (+ (:y this) yinc)))
+   }
+  })
--- test/fp_oo/ch5_class_test.clj
+++ test/fp_oo/ch5_class_test.clj
@@ -1,3 +1,26 @@
 (ns fp-oo.ch5-class-test
   (require [clojure.test :refer :all]
            [fp-oo.ch5-class :refer :all]))
+
+(deftest make-test
+  (testing "can make an 'instance' of an object as a hash with keys"
+    (testing "verbose make"
+      (let [point (verbose-make Point 1 2)]
+        (is (= 'Point (:__class_symbol__ point)))
+        (is (= 1 (:x point)))
+        (is (= 2 (:y point)))))
+    (testing "better make"
+      (let [point (make Point 1 2)]
+        (is (= 'Point (:__class_symbol__ point)))
+        (is (= 1 (:x point)))
+        (is (= 2 (:y point)))))))
+
+(deftest send-to-test
+  (testing "can call methods on instances that don't hold  direct reference to methods"
+
+    (use 'fp-oo.ch5-class)    ;; need this for some namespacing/context issue
+
+    (testing "verbose send-to"
+      (let [point (make Point 1 2)]
+        (is (= 'Point (send-to point :class)))
+        (is (= (make Point -1 -1) (send-to point :shift -2 -3)))))))
