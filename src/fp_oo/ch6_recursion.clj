b8c232dfbde8f4e696561ff375527109c34d83bd
Andrew Melis <andrewmelis@gmail.com>
2015-02-25 22:52:06 -0600

    add ex 1

--- src/fp_oo/ch6_recursion.clj
+++ src/fp_oo/ch6_recursion.clj
@@ -1,1 +1,9 @@
 (ns fp-oo.ch6-recursion)
+
+(def factorial
+  (fn [n]
+    (if (< n 2)
+      1
+      (* n 
+         (factorial 
+           (dec n))))))
--- test/fp_oo/ch6_recursion_test.clj
+++ test/fp_oo/ch6_recursion_test.clj
@@ -1,3 +1,11 @@
 (ns fp-oo.ch6-recursion-test
   (require [clojure.test :refer :all]
            [fp-oo.ch6-recursion :refer :all]))
+
+(deftest factorial-test
+  (testing "single parameter pattern"
+    (is (= 1 (factorial 0)))
+    (is (= 1 (factorial 1)))
+    (is (= 2 (factorial 2)))
+    (is (= 120 (factorial 5)))))
+
