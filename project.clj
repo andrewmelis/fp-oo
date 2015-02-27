b155f0d2e1776979a52c4e2786f4afddd5550bd0
Andrew Melis <andrewmelis@gmail.com>
2015-02-27 11:58:23 -0600

    add midje to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject fp-oo "0.1.0-SNAPSHOT"
   :description "we want the func"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [midje "1.6.3"]])
