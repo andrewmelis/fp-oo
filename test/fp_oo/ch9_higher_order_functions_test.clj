03e97fbb4b8ad7bb15773845e0c5a35af3f92e8e
Andrew Melis <andrewmelis@gmail.com>
2015-03-03 22:09:46 -0600

    add ex1

--- test/fp_oo/ch9_higher_order_functions_test.clj
+++ test/fp_oo/ch9_higher_order_functions_test.clj
@@ -1,2 +1,14 @@
 (ns fp-oo.ch9_higher_order_functions_test
   (require [midje.sweet :refer :all]))
+
+(facts "ex1 - without using fn"
+  (fact "can use partials"
+    (fact "can use a partial on the f passed to map"
+      (map (partial + 2) [1 2 3])
+      => (map (fn [n] (+ 2 n)) [1 2 3]))
+    (fact "can capture the map as well, only needing the collection"
+      ((partial map (partial + 2)) [1 2 3])
+      => (map (fn [n] (+ 2 n)) [1 2 3])))
+  (fact "can use comp to compose two inc functions"
+    (map (comp inc inc) [1 2 3])
+    => (map (fn [n] (+ 2 n)) [1 2 3])))
